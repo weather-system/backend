00a20c5a5e8426f9079d7ffdcb5d7e6d62b778d4
Tazidi <rizki.tazidi@gmail.com>
2024-12-07 11:47:28 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherDataController;
 
 Route::post('weather-data', [WeatherDataController::class, 'store']);
-Route::get('dashboard', [WeatherDataController::class, 'index']);
+Route::get('get/weather-data', [WeatherDataController::class, 'showAll']);
